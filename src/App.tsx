e6def0e5e2f1f6257bb0a40b554551f7b505ed73
VDromaretska <viktoriya.dromaretska@gmail.com>
2023-07-25 12:29:44 +0200

    Format and lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-
 import { EpisodeList } from "./components/EpisodeList";
 import { Footer } from "./components/Footer";
 
 function App(): JSX.Element {
-  
   return (
     <>
       <EpisodeList />;
       <Footer />
     </>
-  )
+  );
 }
 
 export default App;
--- src/components/EpisodeList.tsx
+++ src/components/EpisodeList.tsx
@@ -1,14 +1,13 @@
-import EpisodeView from "./EpisodeView"
-import episodes from "../episodes.json"
+import EpisodeView from "./EpisodeView";
+import episodes from "../episodes.json";
 import "../main.css";
 
-
-
-export function EpisodeList():JSX.Element {
-
-    return (
-        <div className="flex-container">
-            {episodes.map((e)=><EpisodeView episode={e} key={e.id} />)}
-        </div>
-    )
+export function EpisodeList(): JSX.Element {
+  return (
+    <div className="flex-container">
+      {episodes.map((e) => (
+        <EpisodeView episode={e} key={e.id} />
+      ))}
+    </div>
+  );
 }
