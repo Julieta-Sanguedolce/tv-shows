68a468cd09d9e8fcddff26764b23233b75ff0248
Julieta-Sanguedolce <julieta.sanguedolce@gmail.com>
2023-07-25 11:58:38 +0200

    Added footer,some CSS  and finished adding summary

--- src/EpisodeView.tsx
+++ src/EpisodeView.tsx
@@ -1,17 +1,30 @@
 import { IEpisode } from "./IEpisode";
 import { episodeCode } from "./episodeCode";
+import "./main.css";
+
+interface EpisodeViewProps {
+  episode: IEpisode;
+}
 
 export default function EpisodeView({
   episode,
 }: EpisodeViewProps): JSX.Element {
-  return (
-    <>
-      <div>{episode.name}</div>
-      <div>{episodeCode(episode.season, episode.number)}</div>
-    </>
-  );
-}
+  const handleImageClick = () => {
+    window.open(episode.url, "_blank");
+  };
 
-interface EpisodeViewProps {
-  episode: IEpisode;
+  return (
+    <div className="flex-card">
+      <div className="episode-title">
+        {episode.name} - {episodeCode(episode.season, episode.number)}
+      </div>
+      <img
+        src={episode.image.medium}
+        alt=""
+        onClick={handleImageClick}
+        style={{ cursor: "pointer" }}
+      ></img>
+      <p>{episode.summary}</p>
+    </div>
+  );
 }
