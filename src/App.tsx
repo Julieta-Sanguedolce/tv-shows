80fd4b1619af83eec431f54a96d03e5c3bbb4e2a
VDromaretska <viktoriya.dromaretska@gmail.com>
2023-07-26 13:31:19 +0200

    Fetching data from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import { EpisodeList } from "./components/EpisodeList";
 import { Footer } from "./components/Footer";
 import { SearchBar } from "./components/SearchBar";
-import { useState } from "react";
-import episodes from "./episodes.json";
+import { useState, useEffect } from "react";
+// import episodes from "./episodes.json";
 import { filterEpisodes } from "./filterEpisodes";
+import { IEpisode } from "./IEpisode";
 
 function App(): JSX.Element {
-  const [searchedTerm, setSearchedTerm] = useState("");
-  const filteredEpisodes = filterEpisodes(episodes, searchedTerm);
+  const [searchedTerm, setSearchedTerm] = useState<string>("");
+  const [episodes, setEpisodes] = useState<IEpisode[]>([]);
 
+  useEffect(() => {
+    async function loadEpisodes() {
+      const episodesFetch = await fetch(
+        "https://api.tvmaze.com/shows/5/episodes"
+      );
+      const jsonContent: IEpisode[] = await episodesFetch.json();
+      setEpisodes(jsonContent);
+      console.log("Hello");
+    }
+    loadEpisodes();
+  }, []);
+
+  const filteredEpisodes = filterEpisodes(episodes, searchedTerm);
   return (
     <>
       <SearchBar
